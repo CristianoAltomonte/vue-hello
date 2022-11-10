4e562140aa81a3139ce5c689f2ddf6ae792e190f
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-11-10 19:08:13 +0000

    Cambia Immagine e Colore Titolo

--- main.js
+++ main.js
@@ -3,11 +3,38 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
-
 var app = new Vue({
     el: '#root',
     data: {
+        testo: 'text-danger',
         message: 'Hello Vue!',
-        image: 'https://img.lacstatic.it/_pageimg/2022-11-07/2022_photo-2022-11-07-21-12-42.jpg'
+        image: 'https://picsum.photos/id/237/300/300'
+    },
+
+    methods: {
+
+        // Possiamo associare un evento a molti
+        // dei nostri tag come il click su un
+        // bottone, il premere invio all’interno di
+        // un input e tanti altri!
+
+        coloreTesto: function () {
+
+            if (this.testo == 'text-danger') {
+                return this.testo = 'text-primary'
+            }
+            return this.testo = 'text-danger';
+
+        },
+
+        cambioImmagine: function () {
+
+            if (this.image == 'https://picsum.photos/id/237/300/300') {
+                return this.image = 'https://picsum.photos/300/300?grayscale'
+            }
+            return this.image = 'https://picsum.photos/id/237/300/300';
+            
+
+        },
     }
 })
